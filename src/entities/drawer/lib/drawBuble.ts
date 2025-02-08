6286440ebe85671fe3590f4c6d69a1b5e06beed9
Dias1c <dias.kappasov@gmail.com>
2025-02-08 23:27:02 +0500

    feat: draw data bubble

---
+++ src/entities/drawer/lib/drawBuble.ts
@@ -0,0 +1,53 @@
+import type { TFuncGetColor } from "./getFunctionGetColorByDelta";
+
+function drawBubbleSelectionOutline({
+  ctx,
+  x,
+  y,
+  r,
+}: {
+  ctx: CanvasRenderingContext2D;
+  x: number;
+  y: number;
+  r: number;
+}) {
+  ctx.beginPath();
+  ctx.strokeStyle = "#fff";
+  ctx.arc(x, y, r, 0, Math.PI * 2);
+  ctx.stroke();
+  ctx.closePath();
+}
+
+export function drawBuble({
+  ctx,
+  x,
+  y,
+  r,
+  getColor,
+  selected,
+}: {
+  ctx: CanvasRenderingContext2D;
+  x: number;
+  y: number;
+  r: number;
+  getColor: TFuncGetColor;
+  selected?: boolean;
+}): Error | void {
+  ctx.beginPath();
+  ctx.fillStyle = getColor({ opacity: 0.05 });
+  ctx.arc(x, y, r, 0, Math.PI * 2);
+  ctx.fill();
+
+  let max = r * 0.1;
+  for (let i = 1; i < max; i += 1) {
+    const opacity = (1 - i / max) / (r * 0.1);
+    ctx.strokeStyle = getColor({ opacity });
+    ctx.arc(x, y, r - i, 0, Math.PI * 2);
+    ctx.stroke();
+  }
+  ctx.closePath();
+
+  if (selected) {
+    drawBubbleSelectionOutline({ ctx, x, y, r });
+  }
+}
---
+++ src/entities/drawer/lib/index.ts
@@ -0,0 +1,3 @@
+export * from "./getFunctionGetColorByDelta";
+
+export * from "./DrawableDataBubble";
