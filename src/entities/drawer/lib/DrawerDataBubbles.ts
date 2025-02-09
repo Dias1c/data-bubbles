f657066a3307f4d86c5ba54f453bdfaa16794c59
Dias1c <dias.kappasov@gmail.com>
2025-02-09 10:57:21 +0500

    feat: DrawerDataBubbles: moving & resizing bubbles

--- src/entities/drawer/lib/DrawerDataBubbles.ts
+++ src/entities/drawer/lib/DrawerDataBubbles.ts
@@ -7,6 +7,15 @@ type TDataElement = {
   value: number;
 };
 
+type TDrawingData = {
+  prevData?: TDataElement;
+  currentData?: TDataElement;
+  directionX: number;
+  directionY: number;
+  targetR: number;
+  drawer: DrawableDataBubble;
+};
+
 export class DrawerDataBubbles {
   canvas: HTMLCanvasElement;
   scale: number;
@@ -14,7 +23,7 @@ export class DrawerDataBubbles {
 
   dataValuesSum: number = 0;
 
-  dataBubblesMap: Map<string, DrawableDataBubble> = new Map();
+  bublesMap: Map<string, TDrawingData> = new Map();
   _data: TDataElement[] = [];
   public get data(): TDataElement[] {
     return this._data;
@@ -25,33 +34,76 @@ export class DrawerDataBubbles {
       return p + c.value;
     }, 0);
 
-    const { dataBubblesMap, canvas, dataValuesSum } = this;
+    const { bublesMap, canvas, dataValuesSum } = this;
+
+    for (const [, value] of bublesMap) {
+      value.prevData = value.currentData;
+      value.currentData = undefined;
+    }
 
     // TODO: Recognize Bubbles To Delete
-
     const maxSize = Math.min(canvas.width, canvas.height);
-    v.forEach((el) => {
-      const r = maxSize * 0.5 * (el.value / dataValuesSum);
+    v.forEach((data) => {
+      // Calculate Radius
+      const radius = maxSize * 0.5 * (data.value / dataValuesSum);
 
       let image: HTMLImageElement | undefined;
-      if (el.src) {
+      if (data.src) {
         image = new Image();
-        image.src = el.src;
+        image.src = data.src;
       }
 
-      const dataBubble = new DrawableDataBubble({
-        x: Math.random() * (canvas.width - r * 2) + r,
-        y: Math.random() * (canvas.height - r * 2) + r,
-        r: r,
-        image: image,
-        label: el.label,
-        value: `${el.value}`,
-        fontFamily: "Arial",
-        getColor: getFunctionGetColorByDelta({ delta: Math.random() * 2 - 1 }),
-      });
+      let bubble = bublesMap.get(data.label);
+      if (!bubble) {
+        const drawer = new DrawableDataBubble({
+          x: Math.random() * (canvas.width - radius * 2) + radius,
+          y: Math.random() * (canvas.height - radius * 2) + radius,
+          r: 0,
+          image: image,
+          label: data.label,
+          value: `${data.value}`,
+          fontFamily: "Arial",
+          getColor: getFunctionGetColorByDelta({
+            delta: 1,
+          }),
+        });
 
-      dataBubblesMap.set(el.label, dataBubble);
+        bubble = {
+          directionX: Math.random() * 2 - 1,
+          directionY: Math.random() * 2 - 1,
+          targetR: radius,
+          currentData: data,
+          drawer,
+        };
+      } else {
+        bubble.currentData = data;
+
+        // Calculate Delta
+        const prevVal = bubble.prevData?.value ?? 0;
+        const curVal = bubble.currentData?.value ?? 0;
+        let delta = ((curVal / prevVal) * 100 - 100) / 100;
+        console.log(curVal, prevVal, delta);
+        if (!Number.isFinite(delta)) {
+          delta = -1;
+        }
+        bubble.drawer.value = `${data.value}`;
+        bubble.drawer.getColor = getFunctionGetColorByDelta({
+          delta: delta,
+        });
+
+        // Set Target
+        bubble.targetR = radius;
+      }
+
+      bublesMap.set(data.label, bubble);
     });
+
+    for (const [, value] of bublesMap) {
+      if (value.currentData) {
+        continue;
+      }
+      value.targetR = 0;
+    }
   }
 
   constructor({ canvas, scale }: { canvas: HTMLCanvasElement; scale: number }) {
@@ -68,32 +120,68 @@ export class DrawerDataBubbles {
     this.ctx = ctx;
   }
 
+  recalculateBubbleSizes() {
+    const { canvas, bublesMap, dataValuesSum } = this;
+    const maxSize = Math.min(canvas.width, canvas.height);
+
+    for (const [, value] of bublesMap) {
+      const { drawer, currentData } = value;
+      const currentDataValue = currentData?.value ?? 0;
+      const radius = maxSize * 0.5 * (currentDataValue / dataValuesSum);
+      if (drawer.r > radius) {
+        drawer.r = radius;
+      }
+      if (currentDataValue == 0) {
+        continue;
+      }
+
+      value.targetR = radius;
+    }
+  }
+
   draw() {
-    const { ctx, canvas, _data, dataValuesSum, dataBubblesMap } = this;
+    const { ctx, canvas, _data, dataValuesSum, bublesMap } = this;
 
     ctx.beginPath();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fill();
 
-    const maxSize = Math.min(canvas.width, canvas.height);
-
-    _data.forEach((d) => {
-      let image: HTMLImageElement | undefined;
-      if (d.src) {
-        image = new Image();
-        image.src = d.src;
+    for (const [key, bubble] of bublesMap) {
+      const { currentData, drawer } = bubble;
+      if (!currentData && bubble.targetR <= 0 && drawer.r <= 0) {
+        bublesMap.delete(key);
+        continue;
       }
 
-      const dataBubble = dataBubblesMap.get(d.label);
-      if (!dataBubble) {
-        return;
+      // Calculate on Time
+      let deltaR = bubble.targetR - drawer.r;
+      if (deltaR > 0) {
+        deltaR = deltaR / 10;
+      } else if (deltaR < 0) {
+        deltaR = deltaR / 10;
+      }
+      drawer.r += deltaR;
+
+      if (drawer.x - drawer.r < 0) {
+        drawer.x = drawer.r;
+        bubble.directionX *= -1;
+      } else if (canvas.width < drawer.x + drawer.r) {
+        drawer.x = canvas.width - drawer.r;
+        bubble.directionX *= -1;
       }
 
-      // TODO: Move On Idle
-      dataBubble.x = dataBubble.x + Math.random() * 2 - 1;
-      dataBubble.y = dataBubble.y + Math.random() * 2 - 1;
+      if (drawer.y - drawer.r < 0) {
+        drawer.y = drawer.r;
+        bubble.directionY *= -1;
+      } else if (canvas.height < drawer.y + drawer.r) {
+        drawer.y = canvas.height - drawer.r;
+        bubble.directionY *= -1;
+      }
 
-      dataBubble.draw({ ctx });
-    });
+      drawer.x += bubble.directionX;
+      drawer.y += bubble.directionY;
+
+      bubble.drawer.draw({ ctx });
+    }
   }
 }
