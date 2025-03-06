ba7e530aed44871df7ed598baf03d21da4f591bf
Dias1c <dias.kappasov@gmail.com>
2025-03-07 01:55:27 +0500

    refactor: WidgetHeader: remove tabs

--- src/widgets/WidgetHeader/index.tsx
+++ src/widgets/WidgetHeader/index.tsx
@@ -1,39 +1,20 @@
 import img_github from "@/shared/assets/logotypes/github.com-long-white.svg";
 import styles from "./styles.module.css";
 
-import { Tabs } from "@/shared/components/tabs/Tabs";
-import { useState } from "react";
+import { type ReactNode } from "react";
 
-export const WidgetHeader = () => {
-  const [selected, setSelected] = useState("view");
-
+export const WidgetHeader = ({
+  childrenCenter,
+}: {
+  childrenCenter?: ReactNode;
+}) => {
   return (
     <header className={styles.header}>
       <div className={styles.header__logo_block} title="Author: Dias1c">
         <h2 className={styles.header__logo_text}>🫧 Data Bubbles</h2>
       </div>
 
-      <Tabs
-        values={[
-          {
-            label: "View",
-            value: "view",
-          },
-          {
-            label: "Share",
-            value: "share",
-          },
-          {
-            label: "Settings",
-            value: "settings",
-            disabled: true,
-          },
-        ]}
-        selected={selected}
-        onSelect={({ value }) => {
-          setSelected(value);
-        }}
-      />
+      {childrenCenter}
 
       <div className={styles.header__social}>
         <a href="https://github.com/Dias1c/data-bubbles" target="_blank" rel="">
