bf63fe40023d489784b891deb9c4363fa14351d3
Dias1c <dias.kappasov@gmail.com>
2025-03-07 01:53:30 +0500

    feat: add useDrawerDataBubbles

---
+++ src/entities/drawer/hooks/useDrawerDataBubles/index.ts
@@ -0,0 +1,64 @@
+import { useEffect, useRef } from "react";
+import { DrawerDataBubbles } from "../../lib";
+
+const data = [
+  {
+    label: "Alice",
+    value: 500,
+    src: "/favicon.png",
+  },
+  {
+    label: "Bob",
+    value: 500,
+    src: "/favicon.png",
+  },
+  {
+    label: "Carl",
+    value: 500,
+    src: "/favicon.png",
+  },
+];
+
+const data2 = [
+  {
+    label: "Alice",
+    value: 500,
+    src: "/favicon.png",
+  },
+  {
+    label: "Bob",
+    value: 250,
+    src: "/favicon.png",
+  },
+  {
+    label: "Carl",
+    value: 1000,
+    src: "/favicon.png",
+  },
+];
+
+export const useDrawerDataBubbles = () => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const drawerRef = useRef<DrawerDataBubbles | null>(null);
+
+  const setCanvas = (canvas: HTMLCanvasElement) => {
+    canvasRef.current = canvas;
+    if (!drawerRef.current) {
+      const scale = window?.devicePixelRatio ?? 1;
+
+      drawerRef.current = new DrawerDataBubbles({ canvas, scale });
+      return;
+    }
+
+    drawerRef.current.canvas = canvas;
+  };
+
+  useEffect(() => {
+    const drawer = drawerRef.current;
+    if (!drawer) return;
+    drawer.data = data;
+    drawer.data = data2;
+  });
+
+  return { setCanvas, drawerRef };
+};
