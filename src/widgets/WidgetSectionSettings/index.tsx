d1336c53f5f924f874d27ff242ebde075a87abb6
Dias1c <dias.kappasov@gmail.com>
2025-03-09 00:17:36 +0500

    feat: View button in settings

--- src/widgets/WidgetSectionSettings/index.tsx
+++ src/widgets/WidgetSectionSettings/index.tsx
@@ -6,7 +6,8 @@ import {
 import { BlockPartition } from "@/shared/components/blocks/BlockPartition";
 import { Button } from "@/shared/components/buttons/Button";
 import { Subheader } from "@/shared/components/headers/Subheader";
-import { useEffect } from "react";
+import { FieldCheckbox } from "@/shared/components/input_fields/FieldCheckbox";
+import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
 export const WidgetSectionSettings = ({
@@ -14,26 +15,39 @@ export const WidgetSectionSettings = ({
 }: {
   drawer: DrawerDataBubbles;
 }) => {
+  // TODO: save to localstorage
+  const [view, setView] = useState(true);
   const dataBubbles = useDrawerDataBubbles();
 
   useEffect(() => {
     const drawer = dataBubbles.drawerRef.current;
-    if (!drawer) return;
+    if (!drawer || !view) return;
     drawer.startAnimation();
     return () => {
       drawer.stopAnimation();
     };
-  }, []);
+  }, [view]);
 
   return (
     <section className={styles.section}>
-      <section className={styles.section_canvas}>
-        <Subheader title="TODO" subtitle="TODO" />
-        <CanvasDataBubbles {...dataBubbles} />
-      </section>
-      <section className={styles.section_controllers}>
+      {view && (
+        <section className={styles.section_canvas}>
+          <Subheader title="TODO" subtitle="TODO" />
+          <CanvasDataBubbles {...dataBubbles} />
+        </section>
+      )}
+      <section
+        className={styles.section_controllers}
+        style={{
+          width: view ? undefined : "100%",
+        }}
+      >
         <div>
-          View
+          <FieldCheckbox
+            label="View"
+            checked={view}
+            onChange={(e) => setView(e.target.checked)}
+          />
           <Button variant="outlined" loading>
             TODO:Export JSON
           </Button>
