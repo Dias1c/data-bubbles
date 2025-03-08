3ba444a549ef487ce17a8b681df91fba5151b6d7
Dias1c <dias.kappasov@gmail.com>
2025-03-09 00:12:03 +0500

    feat: Typography disabled

--- src/shared/components/typography/Typography/index.tsx
+++ src/shared/components/typography/Typography/index.tsx
@@ -5,14 +5,22 @@ type TVariant = "h3";
 export const Typography = ({
   children,
   variant,
+  disabled,
 }: {
   children?: ReactNode;
   variant?: TVariant;
+  disabled?: boolean;
 }) => {
   if (variant == "h3") return <Heading3>{children}</Heading3>;
-  return <p>{children}</p>;
+  return <p className={disabled ? "disabled" : undefined}>{children}</p>;
 };
 
-const Heading3 = ({ children }: { children?: ReactNode }) => {
-  return <h3>{children}</h3>;
+const Heading3 = ({
+  children,
+  disabled,
+}: {
+  children?: ReactNode;
+  disabled?: boolean;
+}) => {
+  return <h3 className={disabled ? "disabled" : undefined}>{children}</h3>;
 };
