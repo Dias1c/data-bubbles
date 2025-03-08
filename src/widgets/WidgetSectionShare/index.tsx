e413cb88e6ea0e05cdeb94587e5e0cf4e9500738
Dias1c <dias.kappasov@gmail.com>
2025-03-08 11:40:16 +0500

    upd: WidgetSectionShare

--- src/widgets/WidgetSectionShare/index.tsx
+++ src/widgets/WidgetSectionShare/index.tsx
@@ -1,65 +1,7 @@
 import type { DrawerDataBubbles } from "@/entities/drawer";
-import { useEffect, useState } from "react";
-
-// TODO
-// const isMobile =
-
-const BoxDrawerImageDownload = ({
-  drawer,
-  mimeType,
-}: {
-  drawer: DrawerDataBubbles;
-  mimeType: string;
-}) => {
-  const [url, setUrl] = useState<string>();
-
-  useEffect(() => {
-    const url = drawer.canvas.toDataURL("image/png");
-    setUrl(url);
-
-    return () => {
-      setUrl(undefined);
-    };
-  }, [mimeType]);
-
-  return (
-    <div
-      style={{
-        display: "block",
-        borderRadius: "4px",
-        border: "1px solid var(--color-divider)",
-        overflow: "hidden",
-        position: "relative",
-      }}
-    >
-      <img
-        src={url}
-        alt="Data Bubbles"
-        style={{
-          width: "100%",
-        }}
-      />
-      <div
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          zIndex: 1,
-          width: "100%",
-          height: "100%",
-
-          textAlign: "center",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          background: "rgba(0, 0, 0, 0.4)",
-        }}
-      >
-        <p>Click to download</p>
-      </div>
-    </div>
-  );
-};
+import { BlockPartition } from "@/shared/components/blocks/BlockPartition";
+import styles from "./styles.module.css";
+import { BoxDrawerImageDownload } from "./ui/BoxDrawerImageDownload";
 
 export const WidgetSectionShare = ({
   drawer,
@@ -67,16 +9,10 @@ export const WidgetSectionShare = ({
   drawer: DrawerDataBubbles;
 }) => {
   return (
-    <>
-      TODO: Buttons
-      <div
-        style={{
-          maxWidth: "400px",
-          width: "100%",
-        }}
-      >
+    <section className={styles.section}>
+      <BlockPartition label="Download Image">
         <BoxDrawerImageDownload drawer={drawer} mimeType={"image/png"} />
-      </div>
-    </>
+      </BlockPartition>
+    </section>
   );
 };
