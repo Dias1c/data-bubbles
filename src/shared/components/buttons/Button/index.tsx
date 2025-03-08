181b3bde74151aa5eb008bddec8abccb85ca52bf
Dias1c <dias.kappasov@gmail.com>
2025-03-08 13:07:58 +0500

    upd: button loading props

--- src/shared/components/buttons/Button/index.tsx
+++ src/shared/components/buttons/Button/index.tsx
@@ -1,6 +1,7 @@
 import type React from "react";
 import type { ReactNode } from "react";
 import styles from "./styles.module.css";
+import { LoadingCircleSvg } from "../../loading/LoadingCircleSvg";
 
 export type TButtonVariant = "outlined" | "text";
 
@@ -14,18 +15,21 @@ export const Button = ({
   disabled,
   children,
   onClick,
+  loading,
 }: {
   variant?: TButtonVariant;
   disabled?: boolean;
   children?: ReactNode;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  loading?: boolean;
 }) => {
   return (
     <button
       className={variantClassNames[variant]}
-      disabled={disabled}
+      disabled={disabled || loading}
       onClick={onClick}
     >
+      {loading && <LoadingCircleSvg size={14} />}
       {children}
     </button>
   );
