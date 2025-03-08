ea171e6f47113477b6fb6ee40fc2ecaac02c76fd
Dias1c <dias.kappasov@gmail.com>
2025-03-08 12:10:01 +0500

    fix: scroll in CanvasDataBubbles

--- src/entities/drawer/ui/CanvasDataBubbles/index.tsx
+++ src/entities/drawer/ui/CanvasDataBubbles/index.tsx
@@ -40,6 +40,7 @@ export const CanvasDataBubbles = ({
         style={{
           width: "100%",
           height: "100%",
+          display: "block",
         }}
         ref={(ref) => {
           if (ref) setCanvas(ref);
