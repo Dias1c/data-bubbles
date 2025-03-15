164609d689dfc3e70f6e2d3ab40bd0e5787738fb
Dias1c <dias.kappasov@gmail.com>
2025-03-15 11:19:31 +0500

    upd: useTabs

--- src/shared/components/tabs/Tabs/useTabs.ts
+++ src/shared/components/tabs/Tabs/useTabs.ts
@@ -1,29 +1,38 @@
 import { useState } from "react";
 
-type TFuncOnActionCallback = <T>(props: { value?: T }) => Promise<void> | void;
+type TFuncOnActionCallback<T> = (props: { value?: T }) => Promise<void> | void;
+
+type TactionSelect<T> = {
+  onStart?: TFuncOnActionCallback<T>;
+  onSuccess?: TFuncOnActionCallback<T>;
+  onError?: (error: unknown) => Promise<void> | void;
+  onFinish?: TFuncOnActionCallback<T>;
+};
 
 export function useTabs<T>({
   defaultSelected,
   actionSelect,
 }: {
   defaultSelected?: T;
-  actionSelect?: {
-    onStart?: TFuncOnActionCallback;
-    onSuccess?: TFuncOnActionCallback;
-    onError?: (error: unknown) => Promise<void> | void;
-    onFinish?: TFuncOnActionCallback;
-  };
+  actionSelect?: TactionSelect<T>;
 }) {
   const [selected, setSelected] = useState<T | undefined>(defaultSelected);
 
-  const select = async ({ value }: { value?: T }) => {
+  const select = async (
+    { value }: { value?: T },
+    localActionSelect?: TactionSelect<T>
+  ) => {
     try {
+      await localActionSelect?.onStart?.({ value });
       await actionSelect?.onStart?.({ value });
       setSelected(value);
+      await localActionSelect?.onSuccess?.({ value });
       await actionSelect?.onSuccess?.({ value });
     } catch (error) {
+      await localActionSelect?.onError?.({ value });
       await actionSelect?.onError?.(error);
     } finally {
+      await localActionSelect?.onFinish?.({ value });
       await actionSelect?.onFinish?.({ value });
     }
   };
