a49bf8b9f11daaf11bbec25f4f1cfdbead620e47
Dias1c <dias.kappasov@gmail.com>
2025-03-24 19:54:25 +0500

    feat: add photo links

--- src/entities/data-bubbles/lib/examples/index.ts
+++ src/entities/data-bubbles/lib/examples/index.ts
@@ -1,55 +1,76 @@
 import type { IData } from "../../";
 
+/**
+ * @description Example data from, photos taken https://wikipedia.org
+ */
 export const exampleDataBubblesValue: IData = {
   title: "Dogs Top Speed",
   states: [
     {
-      title: "Example",
+      title: "Personal rating example",
       bubbles: [
         {
           name: "Tazy",
           value: 80,
           display_value: "~80km/h",
+          img_src:
+            "https://upload.wikimedia.org/wikipedia/commons/c/ca/Stamp_of_Kazakhstan_Tasy.jpg",
         },
         {
           name: "Greyhound",
           value: 70,
           display_value: "~70km/h",
+          img_src:
+            "https://upload.wikimedia.org/wikipedia/commons/3/38/Greyhound_Racing_2_amk.jpg",
         },
         {
           name: "Saluki",
           value: 67,
           display_value: "67km/h",
+          img_src:
+            "https://upload.wikimedia.org/wikipedia/commons/a/a9/Saluki_dog_breed.jpg",
         },
         {
           name: "Vizsla",
           value: 65,
           display_value: "65km/h",
+          img_src:
+            "https://upload.wikimedia.org/wikipedia/commons/2/2d/Wy%C5%BCe%C5%82_w%C4%99gierski_g%C5%82adkow%C5%82osy_500.jpg",
         },
         {
           name: "Afghan Hound",
           value: 65,
           display_value: "65km/h",
+          img_src:
+            "https://upload.wikimedia.org/wikipedia/commons/e/e8/Afghan_dog_-_cropped.jpg",
         },
         {
           name: "Ibizan Hound",
           value: 65,
           display_value: "65km/h",
+          img_src:
+            "https://upload.wikimedia.org/wikipedia/commons/e/ef/Podenco_z_ibizy_645.jpg",
         },
         {
           name: "Jack Russell Terrier",
           value: 61,
           display_value: "61km/h",
+          img_src:
+            "https://upload.wikimedia.org/wikipedia/commons/6/6a/Jack_russell.jpg",
         },
         {
           name: "Whippet",
           value: 60,
           display_value: "60km/h",
+          img_src:
+            "https://upload.wikimedia.org/wikipedia/commons/7/76/Whippet_2018_6.jpg",
         },
         {
           name: "Dalmatian",
           value: 59,
           display_value: "59km/h",
+          img_src:
+            "https://upload.wikimedia.org/wikipedia/commons/1/1c/Binka_10_06.jpg",
         },
       ],
     },
