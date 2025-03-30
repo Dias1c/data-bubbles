d5049e867d1a66aef5d1bbd9fa0aa672df2988f9
Dias1c <dias.kappasov@gmail.com>
2025-03-31 01:29:57 +0500

    refactor: getFunctionGetColorByDelta

--- src/entities/data-bubbles/lib/drawer/getFunctionGetColorByDelta.ts
+++ src/entities/data-bubbles/lib/drawer/getFunctionGetColorByDelta.ts
@@ -2,38 +2,61 @@ import { mixRgba } from "./mixRgba";
 
 export type TFuncGetColor = (prop: { opacity: number | string }) => string;
 
+interface IRGB {
+  r: number;
+  g: number;
+  b: number;
+}
+
 export function getFunctionGetColorByDelta({
   delta,
+  rgbOnZero,
+  rgbOnNegative,
+  rgbOnPositive,
 }: {
   delta?: number;
+  rgbOnZero?: IRGB;
+  rgbOnNegative?: IRGB;
+  rgbOnPositive?: IRGB;
 }): TFuncGetColor {
   let d = delta ?? 0;
 
+  let rgbZero: IRGB = rgbOnZero ?? {
+    b: 255,
+    g: 255,
+    r: 255,
+  };
+  let rgbNegative: IRGB = rgbOnNegative ?? { b: 0, g: 0, r: 255 };
+  let rgbPositive: IRGB = rgbOnPositive ?? { b: 0, g: 255, r: 0 };
+
   if (!d || d === 0) {
-    return ({ opacity = 1 }) => `rgba(255, 255, 255, ${opacity})`;
+    return ({ opacity = 1 }) =>
+      `rgba(${rgbZero.r}, ${rgbZero.g}, ${rgbZero.b}, ${opacity})`;
   }
 
-  let red = 0;
-  let green = 0;
-
+  let rgb = rgbZero;
   if (d < 0) {
     if (d < -1) {
       d = -1;
     }
     d = d * -1;
 
-    red = 255 * d;
+    rgb = rgbNegative;
   } else if (d > 0) {
     if (d > 1) {
       d = 1;
     }
 
-    green = 255 * d;
+    rgb = rgbPositive;
   }
 
-  const mix = mixRgba(
-    { r: 255, g: 255, b: 255, a: 1, ratio: 1 - d },
-    { r: red, g: green, b: 0, a: 1, ratio: d }
+  rgb.r *= d;
+  rgb.g *= d;
+  rgb.b *= d;
+
+  let mix = mixRgba(
+    { ...rgbZero, a: 1, ratio: 1 - d },
+    { ...rgb, a: 1, ratio: d }
   );
 
   return ({ opacity = 1 }) => `rgba(${mix.r}, ${mix.g}, ${mix.b}, ${opacity})`;
