14be8dbe392aa871388d7a222ee3e735ba3ea3f5
Dias1c <dias.kappasov@gmail.com>
2025-04-06 18:49:38 +0500

    upd: InputText

--- src/shared/components/inputs/InputText/index.tsx
+++ src/shared/components/inputs/InputText/index.tsx
@@ -2,12 +2,20 @@ export const InputText = ({
   disabled,
   value,
   onChange,
+  placeholder,
 }: {
   disabled?: boolean;
   value?: string;
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
+  placeholder?: string;
 }) => {
   return (
-    <input type="text" value={value} onChange={onChange} disabled={disabled} />
+    <input
+      type="text"
+      value={value}
+      onChange={onChange}
+      disabled={disabled}
+      placeholder={placeholder}
+    />
   );
 };
